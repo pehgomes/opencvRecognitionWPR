c0fb01d2e785909dbb4821cb3a08bcfc8f7ab36b
PedroHenriGomes <pedrogomesup2@gmail.com>
2017-06-13 19:42:24 -0300

    add logo

--- ocwp/dLib/MainDblib.py
+++ ocwp/dLib/MainDblib.py
@@ -6,13 +6,13 @@ video_capture = cv2.VideoCapture(0)
 logo = cv2.imread("logo1.png")
 cv2.imshow("logo", logo)
 
-obama_image = face_recognition.load_image_file("eu.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+imagenReconhecida = face_recognition.load_image_file("eu.jpg")
+imagenReconhecida_encoding = face_recognition.face_encodings(imagenReconhecida)[0]
 
 
 face_locations = []
-face_encodings = []
-face_names = []
+face_encodings_filmadas = []
+nome_das_faces = []
 process_this_frame = True
 
 while True:
@@ -21,32 +21,33 @@ while True:
     small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
 
     if process_this_frame:
+        #procurando todos os rostos e encodings na camera
         face_locations = face_recognition.face_locations(small_frame)
-        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+        face_encodings_filmadas = face_recognition.face_encodings(small_frame, face_locations)
 
-        face_names = []
-        for face_encoding in face_encodings:
-            match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        nome_das_faces = []
+        for face_encoding in face_encodings_filmadas:
+            match = face_recognition.compare_faces([imagenReconhecida_encoding], face_encoding)
             name = "desconhecido"
 
             if match[0]:
                 name = "Pedro H. Gomes"
 
-            face_names.append(name)
+            nome_das_faces.append(name)
 
     process_this_frame = not process_this_frame
 
 
 
 
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
+    for (top, right, bottom, left), name in zip(face_locations, nome_das_faces):
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
 
-
+        #desenhando um retangulo ao redor do rosto
         cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255),2)
 
 
